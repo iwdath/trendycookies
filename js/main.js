a1ecebf4ce187235715c80e11a3ce837a7a3e816
iwdath <lazorgrechak@gmail.com>
2024-04-21 01:09:57 +0300

    feat(currency-switcher): implement currency switching functionality
    - Implement a currency switcher feature allowing users to toggle between 
    different currencies for product prices.
    - Update all image alt attributes to Russian, aligning with the primary 
    language of the website for better accessibility and SEO.
    
    The currency switcher is accessible via a fixed UI element on the 
    site, and the alt attributes have been carefully revised to ensure 
    clarity and relevance.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ for (let i = 0; i < btns.length; i++) {
   document.getElementById('order').scrollIntoView({behavior: 'smooth'});
   }
 }
+
+
+const prices = document.getElementsByClassName('product__price');
+document.getElementById('change-currency').onclick = function (e) {
+  const currentCurrency = e.target.innerText;
+
+  let newCurrency = '$';
+  let coefficient = 1;
+  if (currentCurrency === '$') {
+    newCurrency = '₽';
+    coefficient = 90;
+  } else if (currentCurrency === '₽') {
+    newCurrency = 'BYN';
+    coefficient = 3;
+  }
+  e.target.innerText = newCurrency;
+
+  for (let i = 0; i < prices.length; i++) {
+    prices[i].innerText = +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) + ' ' + newCurrency;
+  }
+}
+
+
